f7340d461ee381f21718a550202b281d5e488c7c
Elia Maino <e.maino@foodpanda.com>
2017-08-01 18:40:48 +0200

    fix bounds calculation

--- src/js/c/c_environment.js
+++ src/js/c/c_environment.js
@@ -1,13 +1,14 @@
 import em_module from './environment'
 
 const getNextStateC = em_module.cwrap('getNextState', null, ['number', 'number', 'number', 'number', 'number']);
+const createBounds = em_module.cwrap('createBounds', null, ['number', 'number', 'number']);
 let currentOffset, nextOffset, boundsOffset, width, height, nextStatus, bounds;
 
 export function initializeEnv(currentStatus, w, h) {
 	width = w;
 	height = h;
 	nextStatus = new Uint8Array(width * height).fill(0);
-	bounds = new Uint8Array(width * height).fill(0);
+	bounds = new Uint32Array(height * 2).fill(0);
 	const memoryCurrent = currentStatus.length + currentStatus.BYTES_PER_ELEMENT;
 	const memoryNext = nextStatus.length + nextStatus.BYTES_PER_ELEMENT;
 	const memoryBounds = bounds.length + bounds.BYTES_PER_ELEMENT;
@@ -16,6 +17,8 @@ export function initializeEnv(currentStatus, w, h) {
 	currentOffset = em_module._malloc(memoryNeeded);
 	nextOffset = currentOffset + (width * height);
 	boundsOffset = nextOffset + (width * height);
+
+	createBounds(boundsOffset, width, height);
 }
 
 export function getNextState(currentStatus) {
--- src/js/c/environment.c
+++ src/js/c/environment.c
@@ -3,21 +3,15 @@
 #include <emscripten.h>
 #include <string.h>
 
-int dead = 0;
-int alive = 1;
-
-int minCount = 2;
-int maxCount = 3;
-
 int getCellStatus(int status, int neighboursCount) {
 	switch(neighboursCount) {
 		case 3: 
-			status = alive;
+			status = 1;
 			break;
 		case 2:
 			break;
 		default:
-			status = dead;
+			status = 0;
 	}
 
 	return status;
@@ -32,39 +26,40 @@ int getLineCount(char *current, int column, int boundLeft, int boundRight) {
 	return current[offset] + left + right;
 }
 
-int getNeighboursCount(char *current, int column, int line, int height, int width, char *bounds) {
+int getNeighboursCount(char *current, int column, int line, int height, int *bounds) {
 	int previousLine = (line == 0) ? height : line - 1;
 	int nextLine = (line == height) ? 0 : line + 1;
 
-  	return getLineCount(current, column, bounds[line], bounds[line + width])
-      	+ getLineCount(current, column, bounds[nextLine], bounds[nextLine + width])
-      	+ getLineCount(current, column, bounds[previousLine], bounds[previousLine + width])
+  	return getLineCount(current, column, bounds[line], bounds[line + height])
+      	+ getLineCount(current, column, bounds[nextLine], bounds[nextLine + height])
+      	+ getLineCount(current, column, bounds[previousLine], bounds[previousLine + height])
       	- current[bounds[line] + column];
 }
 
-void createBounds(char *bounds, int width, int height) {
+EMSCRIPTEN_KEEPALIVE
+void createBounds(int *bounds, int width, int height) {
 	int leftBound = 0;
 
 	for (int i = 0; i < height; i++) {
 		leftBound = i * width;
 		bounds[i] = leftBound;
-		bounds[i + width] = leftBound + width - 1;
+		bounds[i + height] = leftBound + width - 1;
 	}
 
 	return;
 }
 
 EMSCRIPTEN_KEEPALIVE
-void getNextState(char *current, char *next, char *bounds, int width, int height) {
+void getNextState(char *current, char *next, int *bounds, int width, int height) {
 	createBounds(bounds, width, height);
 	int maxHeight = height - 1;
 	int index = 0;
 
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
+	for (int line = 0; line < height; line++) {
+		for (int column = 0; column < width; column++) {
 			next[index] = getCellStatus(
 				current[index],
-				getNeighboursCount(current, j, i, maxHeight, width, bounds)
+				getNeighboursCount(current, column, line, maxHeight, bounds)
 			);
 
 			index++;
