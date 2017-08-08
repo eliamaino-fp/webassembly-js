c82f332a8f2145247a591450c6c5449ea23b7c06
Willian Martins <wmsbill@gmail.com>
2017-08-08 11:16:58 +0200

    Fixing CR comments

--- src/js/modules/worker-dispacher.js
+++ src/js/modules/worker-dispacher.js
@@ -10,13 +10,14 @@ export function splitJob(workersNum = 1) {
   return function runJobs(state, columns, lines) {
     let size = columns * lines / workersNum,
       jobs = workers.map(function (run, i) {
-        let limit = i + 1 < workersNum ? size * (i + 1) : columns * lines;
+        let limit = i < workersNum - 1? size * (i + 1) : columns * lines;
         return run([state, columns, lines, size * i, limit]);
       });
 
     return Promise.all(jobs).then(function (pieces) {
       let result = new Uint8Array(columns * lines),
         offset = 0;
+
       pieces.forEach(function (piece) {
         result.set(piece, offset);
         offset += piece.length;
