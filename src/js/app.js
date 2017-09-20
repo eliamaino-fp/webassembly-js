34b61dee64b4b87409ff85b7b3e42fb14196e6af
Willian Martins <wmsbill@gmail.com>
2017-09-20 08:45:26 +0200

    Adding small refactoring to inject the strtegy instead of using it inside Game compoennt (#5)

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,37 @@
 import { game } from './modules/game'
+import { getNextState } from './modules/environment';
+import { cEnvironment } from './modules/c/c_environment';
+import { splitJob } from './modules/worker-dispacher';
 
 const createGameMatrix = (width, height) => new Uint8Array(width * height).fill(0)
   .map(() => Math.ceil(Math.random() * 1000) % 4 ? 0 : 1)
 
 const COLUMNS = 800;
 const LINES = 450;
-const STRATEGY = 'vanilla';
-
-let next = game(
+const STRATEGY = 'workers';
+const initialConfig = createGameMatrix(LINES, COLUMNS);
+const next = game(
   document.getElementById('game'),
   COLUMNS,
   LINES,
-  createGameMatrix(LINES, COLUMNS),
-  STRATEGY
-), i = 0;
+  initialConfig,
+  strategy(STRATEGY, COLUMNS, LINES, initialConfig)
+);
 
-let loop = () => {
-  next().then(() => {
-      requestAnimationFrame(loop);
-  });
+function strategy(strategyType, columns, lines, firstGeneration) {
+  if (strategyType === 'wasm') {
+    return cEnvironment(columns, lines, firstGeneration);
+  } else if (window.Worker && strategyType === 'workers') {
+    return splitJob(2);
+  } else {
+    return getNextState;
+  }
 }
+
+function loop() {
+  next().then(() => {
+    requestAnimationFrame(loop);
+  });
+};
 
 loop();
--- src/js/modules/game.js
+++ src/js/modules/game.js
@@ -1,29 +1,14 @@
-import { getNextState } from './environment';
-import { cEnvironment } from './c/c_environment';
-import { splitJob } from './worker-dispacher';
 import { getRender } from './render';
 
 export function game(elm, columns, lines, initialConfig, strategy) {
   let state = initialConfig,
     render = getRender(elm, columns, lines),
-    nextState;
-
-  if (strategy === 'wasm') {
-    nextState = cEnvironment(columns, lines, initialConfig);
-  } else if (window.Worker && strategy === 'workers') {
-    nextState = splitJob(2);
-  } else {
-    nextState = getNextState;
-  }
+    nextState = strategy;
 
   render(state);
 
   return async function renderState () {
-    if (strategy === 'wasm') {
-      state = await nextState();
-    } else {
-      state = await nextState(state, columns, lines);
-    }
+    state = await nextState(state, columns, lines);
     render(state);
   }
 };
